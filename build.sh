fc043290addbf5dd211da77a9a68cd8494169ccb
Carl-Erik Kopseng <carlerik@gmail.com>
2017-01-04 13:25:00 +0100

    Add configurable port to shell script

--- build.sh
+++ build.sh
@@ -18,8 +18,11 @@ build_jar(){
 
 create_exe(){
     EXE=view-jsp
+
     cat > $EXE << EOF  
+
 #!/bin/sh
+
 function usage(){
     echo "Usage:    \$0 <directory> [properties-file.txt]"
     echo
@@ -30,19 +33,28 @@ function usage(){
     echo "your.prop=2"
     echo "These will then be set as system properties"
 }
+
 if [[ \$# < 1 ]]; then 
     echo "Missing arguments" >> /dev/stderr
     usage
     exit 1
 fi
 
+WEBROOT="\$1"
+
+[[ x\$PORT == x ]] && PORT=9000
+
+# shift arguments by 1 (meaning drop the first)
+shift
+
 # use this shell script as a jar, as the jar file is embedded!
 #java    -Dtomcat.util.scan.StandardJarScanFilter.jarsToScan="taglibs-standard-impl-*"     -Dtomcat.util.scan.StandardJarScanFilter.jarsToSkip="apache-*,ecj-*,jetty-*,asm-*,javax.servlet-*,javax.annotation-*,taglibs-standard-spec-*"  -Dwebroot="\$1" -jar "\$0"
 
 # temporary fix until I can sort out https://github.com/fatso83/jsp-viewer/issues/1
 java    -Dtomcat.util.scan.StandardJarScanFilter.jarsToScan="taglibs-standard-impl-*"     \\
+        -Dport="\$PORT" \
         -Dtomcat.util.scan.StandardJarScanFilter.jarsToSkip="apache-*,ecj-*,jetty-*,asm-*,javax.servlet-*,javax.annotation-*,taglibs-standard-spec-*"  \\
-        -Dwebroot="\$1" -jar "$JAR"
+        -Dwebroot="\$WEBROOT" -jar "$JAR" "\$@"
 
 exit 0
 EOF
